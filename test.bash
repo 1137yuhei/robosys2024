ff13ee2196a1ab8f8f03d0367dce836c0ff7ee8f
mitsuno <s23c1137@s.chibakoudai.jp>
2024-11-07 12:58:17 +0900

    All test

--- test.bash
+++ test.bash
@@ -9,16 +9,7 @@ res=0
 
 ### NOMAL INPUT ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
-
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]　　　|| ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo | ./plus)
-[ "$?" = 1 ]　　　|| ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+[ "${out}" = 15 ] || ng "$LINEMO"
 
 [ "${res}" = 0 ] && echo OK
 exit "$res"
